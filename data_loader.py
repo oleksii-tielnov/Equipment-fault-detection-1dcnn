a03b7f3269bc4879319922b2f49b6c28076ff7e8
Oleksii Tielnov <oleksiitielnov@gmail.com>
2024-10-07 22:17:09 +0200

    update plots

---
+++ data_loader.py
@@ -0,0 +1,23 @@
+from common import get_vec, get_labels, format_to_4_digits
+
+
+def load_data():
+    
+    data = []
+    labels = get_labels()
+    for i in range(1, 100):
+        new_i = format_to_4_digits(i)
+        
+        vec = get_vec(new_i)
+        label = labels[i - 1]
+    
+        entity = [vec, label]
+        data.append(entity)
+
+    return data
+
+
+if __name__ == "__main__":
+    temp = load_data()
+
+    print(type(temp[0]), len(temp[0]), len(temp))
---
+++ model.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+class OneDCNN:
+    def __init__(self) -> None:
+        pass
+
+    def forward():
+        pass
+
+    def SGD():
+        pass
+
+    def backprop():
+        pass
+
+    def update_mini_batch():
+        pass
+
+
+def weight_init():
+    pass
